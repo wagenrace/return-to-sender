408041c202b7d73fe580a5a1fdf83a612bda2b54
wagenrace <nijhof.dns@gmail.com>
2024-02-29 22:02:28 +0100

    make player move like it goes to water

--- character_new.gd
+++ character_new.gd
@@ -1,9 +1,7 @@
 extends CharacterBody3D
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-const FRICTION = 25
-const HORIZONTAL_ACCELERATION = 30
+const HORIZONTAL_ACCELERATION = 3
 const MAX_SPEED=5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -18,8 +16,6 @@ func _ready():
 func _unhandled_input(event):
 	if event is InputEventMouseMotion and Input.mouse_mode==Input.MOUSE_MODE_CAPTURED:
 		rotate_y(-event.relative.x * .005)
-		camera.rotate_x(-event.relative.y * .005)
-		camera.rotation.x = -0.12828170002
 
 func _unhandled_key_input(event):
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -32,10 +28,6 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and Input.mouse_mode==Input.MOUSE_MODE_CAPTURED:
-		velocity.y += JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.

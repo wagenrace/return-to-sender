86b04c96ca029413dbd87bb64326e224be77ea94
wagenrace <nijhof.dns@gmail.com>
2024-03-02 22:24:03 +0100

    track player

--- enemy_ram.gd
+++ enemy_ram.gd
@@ -1,13 +1,16 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 4.5
 const ACCEL = 10
 const JUMP_VELOCITY = 4.5
+var player
 
 @onready var nav: NavigationAgent3D = $NavigationAgent3D
 #@onready var player: CharacterBody3D = $Player
 
+func _ready():
+	player = get_tree().get_root().get_node("World").get_node("Player")
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -15,8 +18,8 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 func _physics_process(delta):
 	
 	var direction = Vector3()
-
-	nav.target_position = Vector3(11.855, 2.0, -14.574)
+	if player:
+		nav.target_position = player.get_position()
 	#nav.target_position = Vector3.ZERO
 #
 	direction = nav.get_next_path_position() - global_position
